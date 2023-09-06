b547df1d5894727f2e82851b1cc0c99e0fb53330
mide david <midebabalola7@gmail.com>
2023-09-06 22:25:19 +0000

    remove comments

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,7 @@
-// Import the Rectangle class
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor(size) {
-    // Call the constructor of the parent class (Rectangle) using super()
     super(size, size);
   }
 }
