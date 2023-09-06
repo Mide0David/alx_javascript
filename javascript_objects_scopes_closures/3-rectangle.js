6851982b2c32897169aaf713f72ca0c094ea281a
mide david <midebabalola7@gmail.com>
2023-09-06 22:14:24 +0000

    remove shebang

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
-#!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        } else {
-            return {};
-
-        }
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If either width or height is not a positive integer, create an empty object
+      return {};
+    } else {
+      // Initialize the instance attributes width and height
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log("x".repeat(this.width));
-        }
+  print() {
+    // Print the rectangle using the character X
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 }
-module.exports = Rectangle
+
+module.exports = Rectangle;
