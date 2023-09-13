28af1573e14a628f0813242576e7523608d73958
mide david <midebabalola7@gmail.com>
2023-09-13 11:25:55 +0000

    status code

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log('Usage: node script.js <URL>');
+  process.exit(1);
+}
+
+const url = args[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  console.log('Code:', response.statusCode);
+});
